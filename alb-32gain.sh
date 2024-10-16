fc6be6e77eaf820e2a7a45b9e044d6efe56571f2
bublyapplejuice2 <hth27@cornell.edu>
2024-10-16 19:01:23 -0400

    alb-32gain.json done

--- alb-32gain.sh
+++ alb-32gain.sh
@@ -1,1 +1,4 @@
-gnpy-transmission-example gnpy/nysernet/alb-32gain.json -e gnpy/nysernet/eqpt_config_gain.json --spectrum gnpy/nysernet/spectrum.json --sim-params gnpy/nysernet/sim_params.json --show-channels 
+gnpy-transmission-example gnpy/nysernet/alb-32gain.json -e gnpy/nysernet/eqpt_config_gain.json --spectrum gnpy/nysernet/spectrum.json --sim-params gnpy/nysernet/sim_params.json --show-channels
+echo "193.35: 14.8"
+echo "193.50: 13.9"
+echo "193.75: 14.1"
