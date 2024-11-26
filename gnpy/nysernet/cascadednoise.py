66b71ebfc7f0eae0d8cb27a79a283dec49ad212d
bublyapplejuice2 <hth27@cornell.edu>
2024-11-26 09:55:59 -0500

    more tuning

---
+++ gnpy/nysernet/cascadednoise.py
@@ -0,0 +1,12 @@
+import math
+
+snr1_db = 24.00
+snr2_db = 17.26
+
+snr1_linear = 10 ** (snr1_db / 10)
+snr2_linear = 10 ** (snr2_db / 10)
+
+snr_total_linear = 1 / ((1 / snr1_linear) + (1 / snr2_linear))
+
+snr_total_db = 10 * math.log10(snr_total_linear)
+print(f"Total SNR: {snr_total_db:.2f} dB")
