bd6bab8b6cce73b00c4ab76615760773a8b2edb6
bublyapplejuice2 <hth27@cornell.edu>
2024-11-26 09:05:27 -0500

    revised model to blue box measured values

--- syr-albgain.sh
+++ syr-albgain.sh
@@ -1,4 +1,1 @@
-gnpy-transmission-example gnpy/nysernet/syr-32gain.json -e gnpy/nysernet/eqpt_config_gain.json --spectrum gnpy/nysernet/syr-albspectrum.json --sim-params gnpy/nysernet/sim_params.json --show-channels
-echo "193.35: 14.8"
-echo "193.50: 13.9"
-echo "193.75: 14.1"
+gnpy-transmission-example gnpy/nysernet/syr-albgain.json -e gnpy/nysernet/eqpt_config_gain.json --spectrum gnpy/nysernet/syr-albspectrum.json --sim-params gnpy/nysernet/sim_params.json --show-channels
